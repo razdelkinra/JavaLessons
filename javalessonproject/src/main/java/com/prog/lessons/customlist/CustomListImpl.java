a76af91ccc2282ba6ceb80757dc4fc4d1253d830
ruslan_razdelkin <rrazdelkin@gmail.com>
2018-05-27 18:37:59 +0300

    Замечания

--- javalessonproject/src/main/java/com/prog/lessons/customlist/CustomListImpl.java
+++ javalessonproject/src/main/java/com/prog/lessons/customlist/CustomListImpl.java
@@ -1,6 +1,5 @@
 package com.prog.lessons.customlist;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
@@ -8,12 +7,14 @@ import java.util.Iterator;
  */
 public class CustomListImpl<T> implements CustomList<T> {
 
-   // Object[] array = new Object[0]; !!! array = ( T[] )new Object[0] + конструктор ; ЧЕМ ОТЛИЧАЮТСЯ? get(index) не принимал в первом случае
+    // Object[] array = new Object[0]; !!! array = ( T[] )new Object[0] + конструктор ; ЧЕМ ОТЛИЧАЮТСЯ? get(index) не принимал в первом случае
 
     private T[] array;
 
+    private int size; // внедрить
+
     public CustomListImpl() {
-        array = ( T[] )new Object[0];
+        array = (T[]) new Object[0];
     }
 
     private static final Object[] DEFAULT_CAPACITY = {};
@@ -22,8 +23,9 @@ public class CustomListImpl<T> implements CustomList<T> {
     public void add(T t) {
         Object[] temp = array;
         array = (T[]) new Object[temp.length + 1];
-        System.arraycopy(temp, 0, array, 0, temp.length);
-        array[array.length-1]= t;
+        System.arraycopy(temp, 0, array, 0, temp.length); // Слишком медленно будет работать,
+        // сделать по другому. При добавлении нового элемента, если он выходит за пределы массива, увеличивать размер вдвое
+        array[array.length - 1] = t;
     }
 
 
@@ -33,23 +35,23 @@ public class CustomListImpl<T> implements CustomList<T> {
         // булин будет если идет удаление по значению.
 
         Object[] temp = array;
-        array = (T[]) new Object[temp.length -1];
+        array = (T[]) new Object[temp.length - 1];
         System.arraycopy(temp, 0, array, 0, index);
         int numMoved = array.length - index - 1;
-        System.arraycopy(temp, index+1, array, index, numMoved);
+        System.arraycopy(temp, index + 1, array, index, numMoved); // Слишком медленно будет работать
 
     }
 
     @Override
     public T get(int index) {
-        // TODO: implements method get.
-
-        return array[index];
+        return array[index]; // верно
     }
 
     @Override
     public int size() {
-        return array.length;
+        return array.length; // сделать отдельную переменную size,
+        // т.к придеться каждый раз при добавлении массива копировать массив,
+        // как ты сделал
     }
 
     @Override
