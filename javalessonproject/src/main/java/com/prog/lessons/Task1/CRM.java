4fe22362ddfb1afc03480100fa157463eea91408
razdelkin roman <razdelkin@gmail.com>
2018-06-07 11:31:51 +0400

    2 задание. с приседанием

--- javalessonproject/src/main/java/com/prog/lessons/Task1/CRM.java
+++ javalessonproject/src/main/java/com/prog/lessons/Task1/CRM.java
@@ -1,7 +1,79 @@
 package com.prog.lessons.Task1;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class CRM {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String ifirstname, ilastname, imobilenumber;
+        String iage;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<Humans> personal = new ArrayList<>();
+
+        System.out.println("1 -Введите нового сотрудника  2 - Список всех сотрудников");
+        String comande = reader.readLine();
+        switch (comande) {
+
+            case "1":
+
+                while (true) {
+
+                    System.out.println("Введите имя сотрудника");
+                    ifirstname = reader.readLine();
+
+                    if (ifirstname.isEmpty())
+                        break;
+
+
+                    System.out.println("Введите фамилию сотрудника");
+                    ilastname = reader.readLine();
+
+                    System.out.println("Введите телефон сотрудника");
+                    imobilenumber = reader.readLine();
+
+                    System.out.println("Введите возраст сотрудника");
+                    iage = reader.readLine();
+                    int iage1 = Integer.parseInt(iage);
+                    personal.add(new Humans(ifirstname, ilastname, imobilenumber, iage1));
+                    Collections.sort(personal, new Humans(ifirstname, ilastname, imobilenumber, iage1));
+
+                }
+
+
+                Writer writer = null;
+                try {
+                    writer = new FileWriter("C:\\Users\\Roman\\IdeaProjects\\JavaLessonsoriginal\\javalessonproject\\src\\main\\resources\\personal.txt",true);
+                    for (Object person : personal) {
+                        writer.write(person + System.getProperty("line.separator"));
+                    }
+
+                } finally {
+                    if (writer != null) {
+                        try {
+                            writer.close();
+                        } catch (IOException ex) {
+                        }
+                    }
+                }
+                break;
+
+            case "2":
+                FileReader fr= new FileReader("C:\\Users\\Roman\\IdeaProjects\\JavaLessonsoriginal\\javalessonproject\\src\\main\\resources\\personal.txt");
+                Scanner scan = new Scanner(fr);
+
+                int i = 1;
+
+                while (scan.hasNextLine()) {
+                    System.out.println(i + " : " + scan.nextLine());
+                    i++;
+                }
+
+                fr.close();
+        }
 
     }
 }
+
--- javalessonproject/src/main/java/com/prog/lessons/Task1/Humans.java
+++ javalessonproject/src/main/java/com/prog/lessons/Task1/Humans.java
@@ -1,4 +1,60 @@
 package com.prog.lessons.Task1;
 
-public class Humans {
+
+import java.util.Comparator;
+
+public class Humans implements Comparator<Humans> {
+
+    private String firstname, lastname, mobilenumber;
+    private int age;
+
+    public Humans(String firstname, String lastname, String mobilenumber, int age) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.mobilenumber = mobilenumber;
+        this.age = age;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getMobilenumber() {
+        return mobilenumber;
+    }
+
+    public void setMobilenumber(String mobilenumber) {
+        this.mobilenumber = mobilenumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String toString() {
+        return this.firstname + " " + this.lastname + " " + this.mobilenumber + " " + this.age;
+    }
+
+
+    @Override
+    public int compare(Humans o1, Humans o2) {
+        return o1.getLastname().compareTo(o2.getLastname());
+    }
 }
+
