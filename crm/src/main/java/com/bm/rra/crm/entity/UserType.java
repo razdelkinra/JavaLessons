76af6ffe84eda692c5c8798011ae86d098652e6e
Ruslan <rrazdelkin@gmail.com>
2018-07-22 17:17:28 +0300

    Create sales manager

---
+++ crm/src/main/java/com/bm/rra/crm/entity/UserType.java
@@ -0,0 +1,27 @@
+package com.bm.rra.crm.entity;
+
+import java.util.Map;
+
+public enum UserType {
+    MANAGER(1, "Менеджер"),
+    SALESPERSON(2, "Продавец"),
+    SERVICEWORKER(3, "Сервисный инженер");
+
+    private String description;
+    private int id;
+
+    UserType(int id, String description) {
+        this.description = description;
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    private static Map<Integer, UserType> userTypes;
+}
