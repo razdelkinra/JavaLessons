6fc56c619be20238674791cf0725419f2e18a4b2
razdelkin roman <razdelkin@gmail.com>
2018-08-05 17:54:22 +0400

    Hello

--- crm/src/main/java/com/bm/rra/crm/controller/HomeController.java
+++ crm/src/main/java/com/bm/rra/crm/controller/HomeController.java
@@ -1,15 +1,20 @@
 package com.bm.rra.crm.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
+    @Value("${welcome.message}")
+    private String message;
 
-    @RequestMapping("/crm")
-    public String home(Model model) {
-        return "index";
+    @RequestMapping(value = {"/", "/hello"}, method = RequestMethod.GET)
+    public String hello(Model model) {
+        model.addAttribute("message", message);
+        return "hello";
     }
 
 }
