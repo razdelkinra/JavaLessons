ec1020692b3f7e433d7e929d37a49ad0e5677a29
Ruslan <rrazdelkin@gmail.com>
2018-08-12 20:56:56 +0300

    Save user method

--- crm/src/main/java/com/bm/rra/crm/controller/UserController.java
+++ crm/src/main/java/com/bm/rra/crm/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.stream.StreamSupport;
 
 @Controller
 public class UserController {
@@ -16,39 +17,25 @@ public class UserController {
     @Autowired
     SalesManagerService salesManagerService;
 
-//    @RequestMapping("/crm/list")
-//    public ModelAndView list(ModelAndView modelAndView) {
-//        SalesManager salesManager = new SalesManager();
-//        salesManager.setFirstName("Roman");
-//        salesManager.setLastName("Razdelkin");
-//        salesManagerService.save(salesManager);
-//        modelAndView.addObject("users", salesManagerService.findAll());
-//        modelAndView.setViewName("crm/list");
-//        return modelAndView;
-//    }
-
-
     @RequestMapping(value = "/crm/list", method = RequestMethod.GET)
     public ModelAndView userList(ModelAndView model) {
         model.setViewName("crm/list");
-        model.addObject("users", salesManagerService.findAll());
+        model.addObject("users",
+                StreamSupport.
+                        stream(salesManagerService.findAll().spliterator(), false).distinct());
         return model;
     }
 
     @RequestMapping(value = "/crm/user", method = RequestMethod.GET)
     public ModelAndView addNewUser(ModelAndView model, @Valid SalesManager user) {
-        model.addObject("firstName", user.getFirstName());
-        model.addObject("lastName", user.getLastName());
         model.setViewName("crm/user");
         model.addObject("user", salesManagerService.save(user));
         return model;
     }
-//    @RequestMapping(value = "/crm/user", method = RequestMethod.POST)
-//    public String addBook(@ModelAttribute("user") SalesManager user) {
-//
-//        salesManagerService.save(user);
-//
-//        return "redirect:/crm/list";
-//
-//    }
+
+    @RequestMapping(value = "/crm/user", method = RequestMethod.POST)
+    public String saveUser(ModelAndView model, @Valid SalesManager user) {
+        salesManagerService.save(user);
+        return "redirect:list";
+    }
 }
